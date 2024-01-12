252f5bce73df4c2cc57660494bdf618ba7803f01
RizalFakhriansy4h <22081000002@student.unmer.ac.id>
2024-01-12 23:31:14 +0700

    haha

---
+++ ci_app/app/Views/about/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>INI ABout</h1>
+    <h1><a href="/">menggunakan / langsung ke home</a></h1>
+    <h1><a href="<?= base_url() ?>">menggunakan base_url home</a></h1>
+</body>
+</html>
---
+++ ci_app/app/Views/home/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <h1>INI HOME</h1>
+    <h1><a href="/about">menggunakan / langsung ke about</a></h1>
+    <h1><a href="<?= base_url("about") ?>">menggunakan base_url about</a></h1>
+</body>
+</html>
